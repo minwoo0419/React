4bdaad72260a066f26edf0af9a89c2c71e64b06e
MinWoo <ghdl464@naver.com>
2023-03-19 19:06:25 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [a, b] = useState('남자 코트 추천');
+  let [c, d] = useState('강남 우동 맛집');
+  let [e, f] = useState('파이썬독학');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4 style={{color: 'red', fontSize : '16px'}}>블로그임</h4>
+      </div>
+      <div className='list'>
+        <h4>{a}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{c}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{e}</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
