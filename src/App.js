b02bafae31f583215d7002f47f5993b5305d0d05
MinWoo <ghdl464@naver.com>
2023-03-21 13:18:28 +0900

    ff

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import { useState } from 'react';
 function App() {
   let [글제목, b] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
   let [따봉, 따봉변경] = useState(0);
+  let copy = [...글제목];
+  const change = () => {
+    if (copy[0] === '남자 코트 추천')
+      copy[0] = '여자 코트 추천';
+    else if (copy[0] === '여자 코트 추천')
+      copy[0] = '남자 코트 추천';
+    b(copy);
+  }
 
   return (
     <div className="App">
@@ -14,14 +22,10 @@ function App() {
         <h4 style={{color: 'red', fontSize : '16px'}}>ReactBlog</h4>
       </div>
       <button onClick={()=>{
-        let copy = [...글제목].sort();
-        b(copy);
+        let cop = [...글제목].sort();
+        b(cop);
       }}> 정렬 </button>
-      <button  onClick={()=>{
-        let copy = [...글제목];
-        copy[0] = '여자 코트 추천';
-        b(copy);
-        }}>글수정</button>
+      <button  onClick={change}>글수정</button>
       <div className='list'>
         <h4> {글제목[0]} <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
@@ -29,7 +33,7 @@ function App() {
       <div className='list'>
         <h4>{글제목[1]}</h4>
         <p>2월 17일 발행</p>
-      </div>>
+      </div>
       <div className='list'>
         <h4>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
